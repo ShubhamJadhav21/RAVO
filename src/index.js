f3c7ac9f08e6b1d4238a28f01b51c222592384e4
Nishnat-9763 <nishnatgupta9763@gmail.com>
2023-11-08 00:55:57 +0530

    cors added

--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const route = require("./routes/user.routes");
 const app = express();
 const { default: mongoose } = require("mongoose");
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.set('strictQuery', true);
-mongoose.connect("mongodb+srv://Nishant:Kh8cI13BDxDiuUHh@cluster0.k0s0qbw.mongodb.net/ravoProject", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
-app.use('/',route)
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(
+    "mongodb+srv://Nishant:Kh8cI13BDxDiuUHh@cluster0.k0s0qbw.mongodb.net/ravoProject",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log('Express app running on port '+ (process.env.PORT || 3000))
-})
+app.use("/", route);
 
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 3000));
+});
