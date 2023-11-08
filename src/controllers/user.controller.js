e1918020bf3ef49d6f7d99d68797c8a2641a12c7
Nishnat-9763 <nishnatgupta9763@gmail.com>
2023-11-08 23:24:03 +0530

    new fields like state,district and taluka added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,40 +11,40 @@ const cloudinary = require('cloudinary').v2;
 const createUser = async function(req, res){
     try {
         let data = req.body;
-        if (Object.keys(data).length == 0) return res.status(400).send({ status: false, error: "Please enter details" });
+        // if (Object.keys(data).length == 0) return res.status(400).send({ status: false, error: "Please enter details" });
 
-        let {firstName,lastName,mobileNumber,vechileType,vechileFuelType,fare,vechileSeater,vechileAcStatus,photos,vechileExtraInfo,state,district,taluka}=data
+        // let {firstName,lastName,mobileNumber,vechileType,vechileFuelType,fare,vechileSeater,vechileAcStatus,photos,vechileExtraInfo,state,district,taluka}=data
 
-        if(!firstName || firstName=="")return res.status(400).send({status:false,error:"firstName is required"})
+        // if(!firstName || firstName=="")return res.status(400).send({status:false,error:"firstName is required"})
 
-        if(!state || state=="")return res.status(400).send({status:false,error:"state is required"})
+        // if(!state || state=="")return res.status(400).send({status:false,error:"state is required"})
 
-        if(!district || district=="")return res.status(400).send({status:false,error:"district is required"})
+        // if(!district || district=="")return res.status(400).send({status:false,error:"district is required"})
         
-        if(!taluka || taluka=="")return res.status(400).send({status:false,error:"taluka is required"})
+        // if(!taluka || taluka=="")return res.status(400).send({status:false,error:"taluka is required"})
 
-        if(!lastName || lastName=="")return res.status(400).send({status:false,error:"lastName is required"})
+        // if(!lastName || lastName=="")return res.status(400).send({status:false,error:"lastName is required"})
 
-        if(!mobileNumber || mobileNumber=="")return res.status(400).send({status:false,error:"mobileNumber is required"})
+        // if(!mobileNumber || mobileNumber=="")return res.status(400).send({status:false,error:"mobileNumber is required"})
 
-        if(!fare || fare=="")return res.status(400).send({status:false,error:"fare is required"})
+        // if(!fare || fare=="")return res.status(400).send({status:false,error:"fare is required"})
 
-        if(!vechileExtraInfo || vechileExtraInfo=="")return res.status(400).send({status:false,error:"vechileExtraInfo is required"})
+        // if(!vechileExtraInfo || vechileExtraInfo=="")return res.status(400).send({status:false,error:"vechileExtraInfo is required"})
 
-        if(!vechileType ||vechileType=="")return res.Status(400).send({status:false, error:"provide vechileType"})
+        // if(!vechileType ||vechileType=="")return res.Status(400).send({status:false, error:"provide vechileType"})
 
-        if(!["seater", "cargoVehicle"].includes(vechileType))return res.status(400).send({status:false, error:"vechileType should be from [seater,cargoVehicle]"})
+        // if(!["seater", "cargoVehicle"].includes(vechileType))return res.status(400).send({status:false, error:"vechileType should be from [seater,cargoVehicle]"})
         
-        if(!vechileFuelType ||vechileFuelType=="")return res.status(400).send({status:false, error:"provide vechileFuelType"})
-        if(!["petrol", "diesel"].includes(vechileFuelType))return res.status(400).send({status:false, error:"vechileFuelType should be from [petrol,diesel]"})
+        // if(!vechileFuelType ||vechileFuelType=="")return res.status(400).send({status:false, error:"provide vechileFuelType"})
+        // if(!["petrol", "diesel"].includes(vechileFuelType))return res.status(400).send({status:false, error:"vechileFuelType should be from [petrol,diesel]"})
         
-        if(!vechileSeater ||vechileSeater=="")return res.status(400).send({status:false, error:"provide vechileSeater"})
-        if(!["swift","ertiga","toyataInnova","bolero","traxCruiser","forceTempoTraveller",].includes(vechileSeater))return res.status(400).send({status:false, error:"vechileSeater should be from [Mr,Mrs,Miss]"})
+        // if(!vechileSeater ||vechileSeater=="")return res.status(400).send({status:false, error:"provide vechileSeater"})
+        // if(!["swift","ertiga","toyataInnova","bolero","traxCruiser","forceTempoTraveller",].includes(vechileSeater))return res.status(400).send({status:false, error:"vechileSeater should be from [Mr,Mrs,Miss]"})
 
-        if(!vechileAcStatus ||vechileAcStatus=="")return res.status(400).send({status:false, error:"provide vechileAcStatus"})
-        if(!["ac","nonAc"].includes(vechileAcStatus))return res.status(400).send({status:false, error:"vechileAcStatus should be from [ac,nonAc]"})
+        // if(!vechileAcStatus ||vechileAcStatus=="")return res.status(400).send({status:false, error:"provide vechileAcStatus"})
+        // if(!["ac","nonAc"].includes(vechileAcStatus))return res.status(400).send({status:false, error:"vechileAcStatus should be from [ac,nonAc]"})
 
-        if(!photos ||photos=="")return res.status(400).send({status:false, error:"provide photos"})
+        // if(!photos ||photos=="")return res.status(400).send({status:false, error:"provide photos"})
         // let cloudUrl
         // let imageUrl =[]
         console.log("hhh",photos.length);
